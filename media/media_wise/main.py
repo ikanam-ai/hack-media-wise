33c89c578bb7a530f5c5e1cac6dc7ab4d0780f0d
Sergey Dorokhov <seregapax@gmail.com>
2024-07-07 12:25:25 +0500

    add stat

--- media/media_wise/main.py
+++ media/media_wise/main.py
@@ -3,10 +3,11 @@ from custom_setup import setup
 
 setup()
 
-
 from form import form
 from map_ import map_
 from calculation import calculate
+from stati import stati
+
 
 
 
@@ -19,6 +20,7 @@ with col2:
     form_data = form()
     if form_data:
         st.session_state['points_count'] = calculate(form_data['points_count'])
+    stati(st.session_state['points_count'])
 
 with col1:
     st_data = map_(st.session_state['points_count'])
---
+++ media/media_wise/stati.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+def stati(points: list[object]) -> None:
+    """Отображение статистики по точкам"""
+    # Создаем Streamlit приложение
+    with st.container():
+        st.subheader("Оптимальные точки")
+        points_text = ""
+        for point in points:
+            if len(point['points']) > 0:
+                coord = point['points'][0]
+                points_text += f"{point['name']}\nШирота: {coord['lat']}\nДолгота: {coord['lon']}\n\n"
+
+        st.text_area("Точки", points_text, disabled=True, height=300)
+
